f81f32de5b1f14b7f3f91820751362cec72a7704
Jehan Fillat <contact@jehanfillat.com>
2021-03-12 17:20:14 +0100

    Add title tag in header.php

--- web/app/themes/jehan.dev-parent/header.php
+++ web/app/themes/jehan.dev-parent/header.php
@@ -5,6 +5,8 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
 
+    <title><?php wp_title(); ?></title>
+
 	<?php wp_head(); ?>
 </head>
 
