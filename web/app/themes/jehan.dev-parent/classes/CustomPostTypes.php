5dfc97ebe857f108f998f1e29c9abd47029fa077
Jehan Fillat <contact@jehanfillat.com>
2021-03-12 17:08:08 +0100

    Add base files for jjgrainger/posttypes implementation

---
+++ web/app/themes/jehan.dev-parent/classes/CustomPostTypes.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace JehanDev;
+
+use PostTypes\PostType;
+
+class CustomPostTypes {
+
+    public function __construct() {
+
+        // Doc here : https://posttypes.jjgrainger.co.uk/
+
+    }
+
+}
--- web/app/themes/jehan.dev-parent/functions.php
+++ web/app/themes/jehan.dev-parent/functions.php
@@ -5,3 +5,4 @@ require_once(dirname(__DIR__, 4) . '/vendor/autoload.php');
 new JehanDev\Optimizations();
 new JehanDev\Security();
 new JehanDev\ThemeSettings();
+new JehanDev\CustomPostTypes();
