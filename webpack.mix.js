f828c817f53c9e9b8a5864c5787b0f75ceb70a08
Jehan Fillat <contact@jehanfillat.com>
2021-11-09 16:59:19 +0100

    Create image build process for child theme
    It automatically creates a .webp version of jpeg and png images

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,9 +1,59 @@
-const mix = require('laravel-mix');
+const mix                       = require('laravel-mix');
+const CopyPlugin                = require("copy-webpack-plugin");
+const ImageMinimizerPlugin      = require("image-minimizer-webpack-plugin");
 
 let child         = process.env.WP_CHILD_THEME,
     buildChild    = child + 'assets/build/',
     distChild     = child + 'assets/dist/'
 ;
+
+mix.webpackConfig({
+    plugins: [
+        new CopyPlugin({
+            patterns: [
+              {
+                from: buildChild + "images",
+                to: './images',
+              },
+            ],
+          }),
+          new ImageMinimizerPlugin({
+            test: /\.(jpe?g|png)$/i,
+            filename: "[path][name].webp",
+            minimizerOptions: {
+              plugins: [
+                    ["imagemin-webp"],
+                    ["imagemin-gifsicle", { interlaced: true }],
+                    ["imagemin-jpegtran", { progressive: true }],
+                    ["imagemin-optipng", { optimizationLevel: 5 }],
+                    // Svgo configuration here https://github.com/svg/svgo#configuration
+                    [
+                        "imagemin-svgo",
+                        {
+                            plugins: [
+                                {
+                                    name: 'preset-default',
+                                    params: {
+                                        overrides: {
+                                            removeViewBox: {
+                                                active: false
+                                            },
+                                            addAttributesToSVGElement: {
+                                                params: {
+                                                    attributes: [{ xmlns: "http://www.w3.org/2000/svg" }],
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            ],
+                        },
+                    ],
+                ],
+            },
+          }),
+    ]
+});
 
 mix.setPublicPath(distChild);
 
